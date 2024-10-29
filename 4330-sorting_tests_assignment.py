1d346abd1c73a04aeec5a5d521ffc9c9aeb13ac7
PeytonAndras <96754073+PeytonAndras@users.noreply.github.com>
2024-10-29 18:18:20 -0500

    sortin algorithm

--- 4330-sorting_tests_assignment.py
+++ 4330-sorting_tests_assignment.py
@@ -1,7 +1,5 @@
 import unittest
 
-
-
 def merge_sort(arr):
     if len(arr) > 1:
         mid = len(arr) // 2
@@ -44,3 +42,35 @@ if __name__ == "__main__":
     merge_sort(arr)
     print("Sorted array is")
     print_list(arr)
+
+class TestMergeSort(unittest.TestCase):
+
+    def test_positive_cases(self):
+        self.assertEqual(merge_sort([12, 11, 13, 5, 6, 7]), [5, 6, 7, 11, 12, 13])
+        self.assertEqual(merge_sort([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6])
+        self.assertEqual(merge_sort([6, 5, 4, 3, 2, 1]), [1, 2, 3, 4, 5, 6])
+
+    def test_negative_cases(self):
+        with self.assertRaises(TypeError):
+            merge_sort(123)
+            merge_sort("123")
+            merge_sort([1, 2, 3, "4", 5, 6])
+
+    def test_performance_cases(self):
+        large_array = list(range(10000, 0, -1))
+        sorted_large_array = list(range(1, 10001))
+        self.assertEqual(merge_sort(large_array), sorted_large_array)
+
+    def test_boundary_cases(self):
+        self.assertEqual(merge_sort([]), [])
+        self.assertEqual(merge_sort([1]), [1])
+        self.assertEqual(merge_sort([1, 2]), [1, 2])
+        self.assertEqual(merge_sort([2, 1]), [1, 2])
+
+    def test_idepotency(self):
+        arr = [12, 11, 13, 5, 6, 7]
+        self.assertEqual(merge_sort(arr), [5, 6, 7, 11, 12, 13])
+        self.assertEqual(merge_sort(arr), [5, 6, 7, 11, 12, 13])
+
+if __name__ == "__main__":
+    unittest.main()
